0f7a725cc7cf70b97e59538f9eb07f4d243d3ec1
erwan.gaychanteloup <erwan.gaychanteloup@gmail.com>
2021-02-11 17:05:20 +0100

    Ajout du bouton de retour à l'accueil

--- src/component/header.svelte
+++ src/component/header.svelte
@@ -2,11 +2,15 @@
     import "@material/mwc-button";
     import "@material/mwc-icon-button";
     import "@material/mwc-top-app-bar";
-    export let title = "";
+    export let title = "";    
+
+    function returnHome(){
+        window.location.href = "/"
+    }
 </script>
 
 <mwc-top-app-bar>
-    <mwc-icon-button icon="menu" slot="navigationIcon"></mwc-icon-button>
+    <mwc-icon-button icon="home" slot="navigationIcon" on:click={returnHome}></mwc-icon-button>
     <div slot="title">{title}</div>
     <mwc-icon-button icon="add" slot="actionItems"></mwc-icon-button>
     <div><!-- content --></div>
